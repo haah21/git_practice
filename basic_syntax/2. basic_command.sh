4f327a88b1b1d09c12be2fd2e80131c0d04ba0a1
getsetgo1 <dnwls1313@gmail.com>
2024-05-29 17:26:24 +0900

    fnfn

--- basic_syntax/2. basic_command.sh
+++ basic_syntax/2. basic_command.sh
@@ -20,5 +20,53 @@ git commit
 # add와 commit 동시에
 git commit -am "add와 commit 동시에"
 
-# 채
+# commit 이력 확인 (local repository의 이력)
+git log
+# git log 간결하게
+git log --oneline
 
+# 전체 이력 조회
+git log --all
+
+# 로그를 그래프 형태로 조회
+git log --graph
+
+# 원격저장소로 업로드
+git push origin 브랜치명
+# 충돌 무시하고 강제 push
+git push origin master --force
+
+# 특정 commit id로의 전환
+git checkout 커밋아이디
+#다시 현재의 master(브렌치)의 commit으로 return
+git checkout master
+
+# git pull은 원격의 변경 사항을 local로 다운로드
+git pull origin 브랜치명
+
+# git 작업 취소
+# working directory에서의 취소
+# 수정 사항만 취소
+git checkout .
+# working directory에서 추가파일 취소
+git clean --fdx
+# working directory 수정사항&추가파일 동시에 취소
+git checkout . | git clean --fdx
+# staging area에서의 취소
+git reset
+# commit 이후의 취소
+git reset HEAD~1
+git reset HEAD^
+# push 이후의 취소
+git revert 커밋ID
+
+
+# 버전별 비교 명령어 : diff
+# A를 기준으로 B가 어떤 변경이 있는지를 비교
+git diff A B
+git diff commitId1 commitId2
+git diff A브랜치 B브랜치
+
+# git fetch
+# 원격에 변경사항을 로컬에 가져오되 병합은 하지 않는 것
+# 비교 : git pull origin master와 git fetch origin master 비교
