6363bf079593936e449145aabdd4db9a2c9bd740
getsetgo1 <dnwls1313@gmail.com>
2024-05-29 14:48:37 +0900

    git 명령어
    # local repository에 반영 및 커밋이력 생성 메시지 내용은 필수값 아님

--- basic_syntax/2. basic_command.sh
+++ basic_syntax/2. basic_command.sh
@@ -1,5 +1,17 @@
 # 현재 working directory, staging area만 상태 확인
 git status
 
+# git add : 모든 수정/추가사항 add
+git add .
+
+# git add 특정파일 : 특정파일 add
+git add 특정파일(위치 포함)
+
 # commit 이력 확인(local repository의 이력)
 git log
+
+
+# local repository에 반영 및 커밋이력 생성 메시지 내용은 필수값 아님
+git commit -m "메시지타이틀" -m "메시지내용"
+
+
