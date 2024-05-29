2073715d513387e02e68b150699e103773ea4534
getsetgo1 <dnwls1313@gmail.com>
2024-05-29 15:08:29 +0900

    add와 commit 동시에

--- basic_syntax/2. basic_command.sh
+++ basic_syntax/2. basic_command.sh
@@ -14,4 +14,11 @@ git log
 # local repository에 반영 및 커밋이력 생성 메시지 내용은 필수값 아님
 git commit -m "메시지타이틀" -m "메시지내용"
 
+# vi모드에서 작성 : git commit 후 엔터
+git commit
+
+# add와 commit 동시에
+git commit -am "add와 commit 동시에"
+
+# 채
 
